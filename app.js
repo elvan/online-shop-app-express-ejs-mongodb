532623488f11ec6532874a47a58417b4d4421d78
Elvan Hidayat <elvanhd@gmail.com>
2024-02-07 00:36:58 +0700

    Integrate user signup and database connection
    - Implement MongoDB connection in app.js for app initialization
    - Add user signup functionality in auth.controller with bcryptjs for password hashing
    - Introduce User model to handle user data and interactions with MongoDB
    - Enable form handling for signup with express.urlencoded middleware
    - Update signup and login views to submit form data correctly
    - Include bcryptjs and mongodb as dependencies in package.json for security and database interaction
    - Setup POST route for signup to process form submission and user creation
    - Modify database.js to establish and provide MongoDB connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
+const db = require('./data/database');
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
@@ -10,7 +11,15 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(authRoutes);
 
-app.listen(3000);
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log('Failed to connect to the database!');
+    console.log(error);
+  });
